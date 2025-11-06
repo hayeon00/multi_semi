c8e38a49a1a92a55a822a49b45d26a86bfcaa266
hayeon00 <rlagkdus1024@naver.com>
2025-11-07 08:06:13 +0900

    초기 세팅

---
+++ src/main/java/com/multi/travel/MultiSemiApplication.java
@@ -0,0 +1,14 @@
+package com.multi.travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiSemiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiSemiApplication.class, args);
+        System.out.println("test");
+    }
+
+}
---
+++ src/test/java/com/multi/travel/MultiSemiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiSemiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
