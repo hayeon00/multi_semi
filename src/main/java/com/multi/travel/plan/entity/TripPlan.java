d2cf4e7a9c9284f77ae3601f719fd283f2eafc77
hayeon00 <rlagkdus1024@naver.com>
2025-11-08 15:54:17 +0900

    Feat(Plan): 여행 기본정보 생성 구현

--- src/main/java/com/multi/travel/plan/entity/TripPlan.java
+++ src/main/java/com/multi/travel/plan/entity/TripPlan.java
@@ -2,34 +2,51 @@ package com.multi.travel.plan.entity;
 
 import com.multi.travel.member.entity.Member;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Please explain the class!!!
- *
- * @author : rlagkdus
- * @filename : TripPlan
- * @since : 2025. 11. 8. 토요일
- */
 @Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TripPlan {
+
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // 여행 제목
+    @Column(nullable = false)
     private String title;
+
+    // 출발 위치
+    @Column(nullable = false)
     private String startLocation;
+
+    // 인원 수
+    @Column(nullable = false)
     private int numberOfPeople;
 
+    // 여행 시작일
+    @Column(nullable = false)
     private LocalDate startDate;
+
+    // 여행 종료일
+    @Column(nullable = false)
     private LocalDate endDate;
 
-    @ManyToOne
+    // 여행 계획 작성자
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "tripPlan", cascade = CascadeType.ALL)
+    // 연결된 코스들
+    @OneToMany(mappedBy = "tripPlan", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Course> courses = new ArrayList<>();
 }
+
--- src/main/java/com/multi/travel/plan/repository/TripPlanRepository.java
+++ src/main/java/com/multi/travel/plan/repository/TripPlanRepository.java
@@ -1,11 +1,15 @@
 package com.multi.travel.plan.repository;
 
+import com.multi.travel.plan.entity.TripPlan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
  * Please explain the class!!!
  *
- * @author : rlagkdus
+ * @author : hayeon
  * @filename : TripPlanRepository
  * @since : 2025. 11. 8. 토요일
  */
-public class TripPlanRepository {
+public interface TripPlanRepository extends JpaRepository<TripPlan, Long> {
+
 }
