220859bf5b0b8627429b5ed026ec834db9fb3cff
hayeon00 <rlagkdus1024@naver.com>
2025-11-10 22:05:07 +0900

    Feat(PlanService): 여행계획 상세조회 구현 day정보 포함x

---
+++ src/main/java/com/multi/travel/course/dto/CoursePlaceDto.java
@@ -0,0 +1,23 @@
+package com.multi.travel.course.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * 코스 목록을 상세조회
+ *
+ * @author : hayeon
+ * @filename : CoursePlaceDto
+ * @since : 2025. 11. 10. 월요일
+ */
+@Builder
+@Getter
+public class CoursePlaceDto {
+    private Long id;          // 장소 ID
+    private String type;      // TOUR_SPOT or ACCOMMODATION
+    private String title;
+    private String address;
+    private String mapx;
+    private String mapy;
+    private Integer orderNo;
+}
