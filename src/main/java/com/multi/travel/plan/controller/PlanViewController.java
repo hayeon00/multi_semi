3b82170a19d17bc3154f78243cf8c6486ea24c71
seunga03 <seungalee220@gmail.com>
2025-11-13 22:26:09 +0900

    Feat(Course, Plan): 코스 수동 생성 페이지 & 코스 수정 페이지 & 계획 상세 페이지 구현 및 코스 상세 페이지 삭제(추후 구현 예정)

--- src/main/java/com/multi/travel/plan/controller/PlanViewController.java
+++ src/main/java/com/multi/travel/plan/controller/PlanViewController.java
@@ -1,8 +1,10 @@
 package com.multi.travel.plan.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -17,9 +19,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/plans/view")
 public class PlanViewController {
 
+    @Value("${KAKAO_MAP_API_KEY}")
+    private String kakaoKey;
+
     @GetMapping("/create")
     public String showCreatePage(@RequestParam(required = false) Long tspId, Model model) {
         model.addAttribute("tspId", tspId);
         return "plan/plan-create"; // templates/plan/plan-create.html
+    }
+
+    @GetMapping("/manual")
+    public String showManualPage(@RequestParam Long planId, Model model) {
+        model.addAttribute("planId", planId);
+        return "course/course-create-manual";
+    }
+
+    @GetMapping("/{planId}")
+    public String showPlanDetailPage(@PathVariable Long planId, Model model) {
+        model.addAttribute("planId", planId);
+        model.addAttribute("kakaoKey", kakaoKey);
+        return "plan/plan-detail";
     }
 }
