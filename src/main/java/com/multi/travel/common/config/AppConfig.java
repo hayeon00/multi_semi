dcafa16c7abfb745f49bad288a1952e649ebc70c
hayeon00 <rlagkdus1024@naver.com>
2025-11-14 19:47:33 +0900

    Feat(ReviewService): 리뷰 등록화면 구현 -리뷰등록시 이미지 같이 등록됨

--- src/main/java/com/multi/travel/common/config/AppConfig.java
+++ src/main/java/com/multi/travel/common/config/AppConfig.java
@@ -11,11 +11,19 @@ package com.multi.travel.common.config;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @Getter
 public class AppConfig {
     @Value("${kakao.map.api-key}")
     private String kakaoMapApiKey;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
 }
